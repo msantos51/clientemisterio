0884683fa35c4a4781d3b8076b4aec75d5184118
msantos51 <miguel_santos1994@hotmail.com>
2025-09-04 14:50:05 +0100

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
+// app/api/auth/[...nextauth]/route.ts
 // Handler do NextAuth para rotas de autenticação
 import NextAuth from 'next-auth'
-import { authOptions } from '../../../../lib/auth'
+import { authOptions } from '@/lib/auth' // usa alias "@" se tiveres baseUrl configurado no tsconfig.json
 
 const handler = NextAuth(authOptions)
 
